a93d13abe064091cba9b106842dc9ad836df8228
Evelin Romero <Evelinrocio2816@gmail.com>
2023-11-17 15:13:15 -0300

    se agrego componentes Search + Footer

--- src/SearchInput/SearchInput.jsx
+++ src/SearchInput/SearchInput.jsx
@@ -1,12 +1,13 @@
 import "./SearchInput.css"
+import { PeopleFill } from 'react-bootstrap-icons'
 
 const SearchInput = () => {
   return (
     <div className="container-Search">
-   
-    <input type="text" placeholder="🔍Search"/>
+  
+    <input  type="text" placeholder="🔍Search"/>
     <button type="submit">Buscar</button>
-
+ <p><PeopleFill/> Login</p>
  </div>
 )
 }
