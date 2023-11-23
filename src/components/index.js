f40552fb92b1831c7555c5a52c4b8ee78ba2c871
FacuCampos <facujcampos@gmail.com>
2023-11-23 11:03:03 -0300

    Se actualizo el index de components

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,8 @@
-import Counter from "./Counter/Counter";
-import NavBar from "./NavBar/NavBar";
-import CartWidget from "./CartWidget/CartWidget";
-import Footer from "./Footer/Footer";
-import SearchInput from "../SearchInput/SearchInput";
-import Login from "./Login/Login";
-import Details from "./Details/Details";
+export { default as Counter } from "./Counter/Counter";
+export { default as Navbar } from "./NavBar/NavBar";
+export { default as CartWidget } from "./CartWidget/CartWidget";
+export { default as Footer } from "./Footer/Footer";
+export { default as SearchInput } from "../SearchInput/SearchInput";
+export { default as Login } from "./Login/Login";
+export { default as Details } from "./Details/Details";
 
-export { Counter, NavBar, CartWidget, Footer, SearchInput, Login,Details };
