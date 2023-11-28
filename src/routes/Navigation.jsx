07a769880456303020018cbe836af5cd9fddbed6
Evelin Romero <Evelinrocio2816@gmail.com>
2023-11-27 21:23:08 -0300

    se agrega checkout pages

--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -1,11 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages";
+import CheckOut from "../pages/CheckOut";
 
 const routes = createBrowserRouter([
     {
         path: '/',
         element: <Home/>
+    },
+    {
+        path:'/checkout',
+        element:<CheckOut/>
     }
+
 ])
 
 const Navigation = () => {
