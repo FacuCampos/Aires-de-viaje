591f55a78f64133332069203b4e41425f52a11ce
Oscarvallo <oscar.carvallo.mkt@gmail.com>
2023-12-21 21:42:24 -0300

    Armando el diseño responsive de linksSection casi está falta pulir

--- src/components/LinksSection/index.jsx
+++ src/components/LinksSection/index.jsx
@@ -1,36 +1,40 @@
-import { AirplaneFill, BriefcaseFill, BuildingsFill, CarFrontFill, HouseFill, HousesFill } from "react-bootstrap-icons"
-import { Link } from "react-router-dom"
-
+import { AirplaneFill, BriefcaseFill, BuildingsFill, CarFrontFill, HouseFill, HousesFill } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
 
 const LinksSection = () => {
-    return (
-        <>
-            <Link to={'/'} className='navLink'>
-                <HouseFill className='navIcon' />
-                Home
-            </Link>
-            <Link to={"/vuelos"} className='navLink'>
-                <AirplaneFill className='navIcon' />
-                Vuelos
-            </Link>
-            <Link to={"/hospedajes"} className='navLink'>
-                <BuildingsFill className='navIcon' />
-                Hospedajes
-            </Link>
-            <Link to={'/alquileres'} className='navLink'>
-                <HousesFill className='navIcon' />
-                Alquileres
-            </Link>
-            <Link to={'/paquetes'} className='navLink'>
-                <BriefcaseFill className='navIcon' />
-                Paquetes
-            </Link>
-            <Link to={'/autos'} className='navLink'>
-                <CarFrontFill className='navIcon' />
-                Autos
-            </Link>
-        </>
-    )
+  return (
+    <div className="LinksSectionGroup">
+      <Link to={'/'} className='container navLink'>
+        <HouseFill className='navIcon' />
+        Home
+      </Link>
+
+      <Link to={"/vuelos"} className='container navLink'>
+        <AirplaneFill className='navIcon' />
+        Vuelos
+      </Link>
+
+      <Link to={"/hospedajes"} className='container navLink'>
+        <BuildingsFill className='navIcon' />
+        Hospedajes
+      </Link>
+
+      <Link to={'/alquileres'} className='container navLink'>
+        <HousesFill className='navIcon' />
+        Alquileres
+      </Link>
+
+      <Link to={'/paquetes'} className='container navLink'>
+        <BriefcaseFill className='navIcon' />
+        Paquetes
+      </Link>
+
+      <Link to={'/autos'} className='container navLink'>
+        <CarFrontFill className='navIcon' />
+        Autos
+      </Link>
+    </div>
+  );
 }
 
-export default LinksSection
+export default LinksSection;
