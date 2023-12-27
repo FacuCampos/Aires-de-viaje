5d8d7d8adc23e49adbb1b599dd8c2487187c87c8
Oscarvallo <oscar.carvallo.mkt@gmail.com>
2023-12-26 21:13:50 -0300

    Agregando el formulario con estilos al Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,39 @@
-import { Layout } from "../../components"
-import './Home.css'
+import { Layout } from "../../components";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Asegúrate de importar los estilos de Bootstrap
+import { BiMap, BiCalendar, BiUser } from 'react-icons/bi'; // Importa los iconos de Bootstrap
 
 const Home = () => {
   return (
     <Layout>
-        <div><h1 className="title">Descubri tu viaje soñado</h1></div>
-        <div className="Img-home" >
-      
-          <h1 className="titleHome">Vuelos baratos, compará y pagá menos por viajar</h1>
+      <div>
+        <h1 className="title">Descubrí tu viaje soñado</h1>
+      </div>
+      <div className="Img-home">
+        <div className="search-form">
+          <div className="form-group">
+            <BiMap className="iconForm"/>
+            <input className='form' type="text" placeholder="Ubicación" />
+          </div>
+          <div className="form-group">
+            <BiCalendar className="iconForm"/>
+            <input className='form' type="date" placeholder="Fecha de entrada" />
+          </div>
+          <div className="form-group">
+            <BiCalendar className="iconForm"/>
+            <input className='form' type="date" placeholder="Fecha de salida" />
+          </div>
+          <div className="form-group">
+            <BiUser className="iconForm"/>
+            <input className='form' type="number" placeholder="Cantidad de personas" />
+          </div>
+          <button className="btnForm" type="submit">Buscar</button>
         </div>
-        <div className="logos-home">
-          <h4>Compará cientos de sitios de viajes en una sola búsqueda</h4>
-{/*           <img className="imgLogos" src="./Image/tuismoLogo.jpeg" alt="turismocity" />
-          <img className="imgLogos" src="./Image/avantrip.png" alt="avantrip" />
-          <img className="imgLogos" src="./Image/Flybondi.png" alt="flybondi" />
-          <img className="imgLogos" src="./Image/volala.jpg" alt="volala" />
-          <img className="imgLogos" src="./Image/atrapalo.png" alt="atrapalo" />
-          <img className="imgLogos" src="./Image/aerolineas-argentina.jpg" alt="aerolineas argentinas" />
- */}
-        </div>
+      </div>
+      <div className="logos-home">
+        <h4>Compará cientos de sitios de viajes en una sola búsqueda</h4>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
